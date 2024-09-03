51bccf5a22ed54e5595b6d03c87300bd6d02a8de
D.J. Crosby <taladan@gmail.com>
2024-09-02 20:30:10 -0400

    feat: Initial commit of hashmap object
    Outline API for hashmap object

---
+++ lib/hashmap.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+# lib/hashmap.rb
+class hashmap
+  def initialize
+    @unused
+  end
+
+  def hash(key)
+    # hash logic
+  end
+
+  # set value to key
+  def set(key, value)
+    # set logic
+  end
+
+  # return value for key
+  def get(key)
+    # get logic
+  end
+
+  # boolean if has key in hashmap
+  def has?(key)
+    # has? logic
+  end
+
+  # remove key from hash and return deleted entry's value
+  def remove(key)
+    # remove logic
+  end
+
+  # return the number of keys stored in the hashmap
+  def length
+    # length logic
+  end
+
+  # remove all entries in hashmap
+  def clear
+    # clear logic
+  end
+
+  # return array of all keys in hashmap
+  def keys
+    # keys logic
+  end
+
+  # return array of all values in hashmap
+  def values
+    # values logic
+  end
+
+  # return array containing each key/value pair [[first_key, first_value], [second_key, second_value]...]
+  def entries
+    # entries logic
+  end
+  
+end
