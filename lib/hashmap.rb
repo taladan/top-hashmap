505a7ea5a5a87ab814d7c538f9036abd69650835
D.J. Crosby <taladan@gmail.com>
2024-09-06 07:21:34 -0400

    feat: #clear removes all entries from hashmap
    This makes hashmap feature complete as required by TOP assignment

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -5,7 +5,7 @@ require_relative 'hasher'
 
 # lib/hashmap.rb
 class Hashmap
-  attr_reader :buckets
+  attr_reader :buckets, :capacity, :load_factor
 
   def initialize
     @capacity = 16
@@ -16,11 +16,10 @@ class Hashmap
 
   # set value to key
   def set(key, value)
-    grow if @buckets.length == @capacity * @load_factor
+    grow if length > @capacity * @load_factor
     hsh = @hasher.make_hash(key)
     index = index_from_hash(hsh)
 
-    # this feels kludgy
     @buckets[index].nil? ? new_bucket(hsh, index, key, value) : @buckets[index].contents.append(key, value)
   end
 
@@ -107,27 +106,39 @@ class Hashmap
   end
 
   # remove all entries in hashmap
-  # unfinished
   def clear
-    # Need to check to see if this will cause linked list in old buckets to remain in memory
-    @buckets = nil
+    @buckets.each do |bucket|
+      next if bucket.nil?
+
+      # We're just dumping values, no need to save them
+      bucket.keys.each do |_key|
+        next if bucket.nil?
+
+        _ = bucket.contents.pop
+      end
+    end
+    @capacity = 16
+    @buckets = Array.new(@capacity)
   end
 
   private
 
   # increase the capacity of the hashmap
-  # unfinished
   def grow
     # expand capacity
     @capacity *= 2
 
-    # initiate new empty array
-    new_buckets_array = Array.new(@capacity)
+    old_buckets = @buckets
+    @buckets = Array.new(@capacity)
 
     # rehash keys and reindex to new capacity
+    old_buckets.each do |entry|
+      next if entry.nil?
 
-    # final step assign array to instance var
-    @buckets = new_buckets_array
+      entry.keys_and_values.each do |pair|
+        set(*pair)
+      end
+    end
   end
 
   # use hash mod capacity to get index number
